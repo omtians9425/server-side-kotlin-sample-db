0e0e897df77faf53d73ae287c5c7fb2c38940ffd
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-12-27 14:26:01 +0900

    implement other queries and pass test

--- src/test/kotlin/com/example/sample_serverside_kotlin_db/demo_db/controllers/ControllerTests.kt
+++ src/test/kotlin/com/example/sample_serverside_kotlin_db/demo_db/controllers/ControllerTests.kt
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -40,5 +41,37 @@ class ControllerTests {
         mockMvc.perform(post("/add").param("name", "unit_test"))
                 .andExpect(status().isOk)
                 .andExpect(content().string("Saved"))
+    }
+
+    // insert subject under the test record before tests.
+    @Test
+    @Sql(statements = ["INSERT INTO user (name) VALUES ('update_data');"])
+    fun updateUserTest() {
+        val lastUser = target.userRepository.findAll().last()
+
+        mockMvc.perform(
+                post("/update")
+                        .param("id", lastUser.id.toString())
+                        .param("name", "updated!!!"))
+                .andExpect(status().isOk)
+                .andExpect(content().string("Updated"))
+
+    }
+
+    @Test
+    fun delete_notExistsUser_returnNoUser() {
+        mockMvc.perform(post("/delete").param("id", "-1"))
+                .andExpect(status().isOk)
+                .andExpect(content().string("User does not exist"))
+    }
+
+    @Test
+    @Sql(statements = ["INSERT INTO user (name) VALUES ('delete_data');"])
+    fun delete_existsUser_returnDeleted() {
+        val lastUser = target.userRepository.findAll().last()
+
+        mockMvc.perform(post("/delete").param("id",lastUser.id.toString()))
+                .andExpect(status().isOk)
+                .andExpect(content().string("Deleted"))
     }
 }
